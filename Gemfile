19e634da0f8622d238626ed017350f38caaad38f
Joel Brewer <joelbrewer01@gmail.com>
2014-01-21 00:31:41 -0600

    use title for filename. add exception handling to fetching

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'pubmed_search'
+gem 'mechanize'
+gem 'bio', :git => 'git://github.com/paulleader/bioruby.git'
+
--- scraper.rb
+++ scraper.rb
@@ -1,47 +1,60 @@
 require 'fileutils'
 require 'rubygems'
-require 'bio'
 require 'pubmed_search'
 require 'mechanize'
+require 'bio'
 
 @search_query = "\"therapeutic misconception\"[All Fields] AND English[lang]"
 @email = "joelbrewer01@gmail.com"
 
+class URLException < Exception
+end
 
 def run
-  pmids = get_pmids(@search_query).each do |pmid|
-    a = Mechanize.new
-
-    article_page = return_article_page_given_pmid(pmid, a)
-
-    free_article_page = check_for_free_article(article_page) || nil
-
-    if free_article_page
-      pdf_link = free_article_page.link_with(:href => /.*pdf.*/) || nil
-      if pdf_link
-        a.get(pdf_link.uri.to_s).save_as "#{pdf_link.to_s}"
-        File.open("downloaded.txt", 'a') { |f| f.write("#{pmid} -- #{pdf_link.to_s}") }
-      end
-    else
-      File.open("not_downloaded.txt", 'a') { |f| f.write("#{pmid}") }
-    end
-  end
+  Bio::NCBI.default_email = "joelbrewer01@gmail.com"
+  pmids = get_pmids(@search_query)
+  download_pdfs_given_pmids(pmids)
 end
 
 def get_pmids(query)
   PubmedSearch::search(query).pmids
 end
 
-def return_article_page_given_pmid(pmid, a)
-  a.get("http://www.ncbi.nlm.nih.gov/pubmed/#{pmid}/")
+def download_pdfs_given_pmids(pmids)
+  pmids.each do |pmid|
+    download_pdf_given_pmid(pmid)
+  end
 end
 
-def check_for_free_article(page)
-  if page.link_with(:text => 'Free PMC Article')
-    return page.link_with(:text => 'Free PMC Article').click
-  else
-    return false
+def download_pdf_given_pmid(pmid)
+  a = Mechanize.new
+  a.get("http://www.ncbi.nlm.nih.gov/pubmed/#{pmid}/") do |page|
+    download_pdf_from_page(page, a, pmid)
   end
+end
+
+def download_pdf_from_page(page, a, pmid)
+  article_page = page.link_with(:text => 'Free PMC Article') || nil
+    if article_page
+      article_page = page.link_with(:text => 'Free PMC Article').click
+      #search for pdf link
+      pdf_link = article_page.link_with(:href => /.*pdf.*/) || nil
+
+      if pdf_link
+        # save pdf
+        begin
+          a.get(pdf_link.uri.to_s).save_as("#{get_paper_title_given_pmid(pmid)}.pdf")
+        rescue 
+          return
+        end
+      end
+    end
+end
+
+def get_paper_title_given_pmid(pmid)
+  article = Bio::PubMed.efetch(pmid)
+  article[0].to_s.match(/[T][I]\s*[-]\s*(.*)$/)[1].gsub(' ','_')
+  
 end
 
 run
