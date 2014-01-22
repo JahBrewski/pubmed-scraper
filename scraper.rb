6152123c82d0b5da25817585923f500cb7cd6700
Joel Brewer <joelbrewer01@gmail.com>
2014-01-21 21:44:27 -0600

    add downloaded and not-downloaded text files. refactor scraping to eliminate nested ifs and make it a little more professional...

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,9 @@ require 'bio'
 
 @search_query = "\"therapeutic misconception\"[All Fields] AND English[lang]"
 @email = "joelbrewer01@gmail.com"
+@downloaded = 0
+@not_downloaded = 0
+
 
 class URLException < Exception
 end
@@ -24,31 +27,45 @@ def download_pdfs_given_pmids(pmids)
   pmids.each do |pmid|
     download_pdf_given_pmid(pmid)
   end
+  File.open("downloaded.txt", 'a') { |f| f.write("\nTotal Downloaded: #{@downloaded}") }
+  File.open("not-downloaded.txt", 'a') { |f| f.write("\nTotal Not Downloaded: #{@not_downloaded}") }
 end
 
 def download_pdf_given_pmid(pmid)
   a = Mechanize.new
   a.get("http://www.ncbi.nlm.nih.gov/pubmed/#{pmid}/") do |page|
-    download_pdf_from_page(page, a, pmid)
+    title = get_paper_title_given_pmid(pmid)
+    
+    if download_pdf_from_page(page, a, pmid) != nil
+      File.open("downloaded.txt", 'a') { |f| f.write("\n#{pmid} - #{title}") }
+      @downloaded = @downloaded + 1
+    else
+      File.open("not-downloaded.txt", 'a') { |f| f.write("\n#{pmid} - #{title}") }
+      @not_downloaded = @not_downloaded + 1
+    end
   end
 end
 
 def download_pdf_from_page(page, a, pmid)
-  article_page = page.link_with(:text => 'Free PMC Article') || nil
-    if article_page
-      article_page = page.link_with(:text => 'Free PMC Article').click
-      #search for pdf link
-      pdf_link = article_page.link_with(:href => /.*pdf.*/) || nil
 
-      if pdf_link
-        # save pdf
-        begin
-          a.get(pdf_link.uri.to_s).save_as("#{get_paper_title_given_pmid(pmid)}.pdf")
-        rescue 
-          return
-        end
-      end
+  article_page = page.link_with(:text => 'Free PMC Article') || nil
+
+  if article_page
+    article_page = page.link_with(:text => 'Free PMC Article').click
+  else return nil
+  end
+
+  #search for pdf link
+  pdf_link = article_page.link_with(:href => /.*pdf.*/) || nil
+
+  if pdf_link
+    begin
+      a.get(pdf_link.uri.to_s).save_as("#{get_paper_title_given_pmid(pmid)}.pdf")
+    rescue 
+      return nil
     end
+  else return nil
+  end
 end
 
 def get_paper_title_given_pmid(pmid)
